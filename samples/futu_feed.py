29bd9475496061296a630a2b042d41410a77f992
Server <aaronzhenzhang@gmail.com>
2018-09-29 18:40:30 +0800

    Fert

--- samples/futu_feed.py
+++ samples/futu_feed.py
@@ -2,19 +2,21 @@ from selenium import webdriver  #导入Selenium的webdriver
 from selenium.webdriver.common.keys import Keys  #导入Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
-from time import *
-from datetime import *
+import time
+#from datetime import *
+import datetime
 
 link = "https://seed.futunn.com/"
 File_Acc = "./acc.txt"
 
 def log(str):
-    now_time = datetime.now()
-    now_time_str = datetime.strftime(now_time, '%Y-%m-%d %H:%M:%S')
+    now_time = datetime.datetime.now()
+    now_time_str = datetime.datetime.strftime(now_time, '%Y-%m-%d %H:%M:%S')
     localtime = now_time_str.split(' ')[1]
     print("[" + localtime + "]" + str)
 
 def do_work(acc, passwd):
+    load_num = 18
     #driver = webdriver.PhantomJS()
     driver = webdriver.Chrome()
     driver.get(link)
@@ -24,36 +26,48 @@ def do_work(acc, passwd):
 
     email_e = login_form.find_element_by_xpath("//*[@class='ui-input-wrapper ui-content-email']")
     email_input = email_e.find_element_by_name("email").send_keys(acc)
-    log("Input Email")
+    #log("Input Email")
     passwd_e = login_form.find_element_by_xpath("//*[@class='ui-input-wrapper']")
     passwd_input = passwd_e.find_element_by_name("password").send_keys(passwd)
-    log("Input Password")
+    #log("Input Password")
 
     btn = driver.find_element_by_xpath("//*[@class='ui-submit ui-form-submit']")
     btn.click()
     log("Log in")
 
-    sleep(5)
+    time.sleep(5)
 
     #### Main Page
     pg_src = driver.page_source
     #print(pg_src)
 
+    #### Pour water
+    try:
+        # waterProgress = driver.find_element_by_class_name("waterProgress")
+        # circle = waterProgress.find_elements_by_tag_name("circle")
+        # time = float(circle[1].get_attribute('stroke-dasharray').split(" ")[0])
+        log("Check water process ")
 
+        waterCanvas = driver.find_element_by_id("waterCanvas")
+        driver.execute_script("arguments[0].click();", waterCanvas)
+        # ActionChains(driver).double_click(waterCanvas).perform()
+        time.sleep(5)
+    except:
+        log("FULL")
 
     #### Friends
     friends_ = driver.find_element_by_xpath("//*[@class='nav-icon icon_nav-friends']")
     friends_.click()
     log("Find Friends")
-    sleep(5)
+    time.sleep(5)
 
 
     ######## Load More
-    for i in range(0, 10):
+    for i in range(0, load_num):
         hasMore_region = driver.find_element_by_xpath("//*[@ng-show='!friendsLoading']")
         driver.execute_script("arguments[0].click();", hasMore_region)
-        log("More Friends")
-        sleep(5)
+        #log("More Friends")
+        time.sleep(5)
 
     friends_need = True
     while friends_need == True:
@@ -68,51 +82,34 @@ def do_work(acc, passwd):
             back = driver.find_element_by_xpath("//*[@class='back-text']")
             driver.execute_script("arguments[0].click();", back)
             log("Back")
-            sleep(5)
+            time.sleep(5)
             break
         log("Someone needs")
         if friends_need == True:
-            sleep(5)
+            time.sleep(5)
             #### Fert
             fert = driver.find_element_by_xpath("//*[@class='opIcon icon_fert']")
             fert.click()
             log("Fert")
-            sleep(5)
+            time.sleep(5)
             #### Back
             back = driver.find_element_by_xpath("//*[@class='back-text']")
             driver.execute_script("arguments[0].click();", back)
             log("Back")
-            sleep(5)
+            time.sleep(5)
             ######## Load More
-            for i in range(0, 10):
+            for i in range(0, load_num):
                 hasMore_region = driver.find_element_by_xpath("//*[@ng-show='!friendsLoading']")
                 driver.execute_script("arguments[0].click();", hasMore_region)
-                log("More Friends")
-                sleep(5)
-    #### Pour water
-    try:
-        #waterProgress = driver.find_element_by_class_name("waterProgress")
-        #circle = waterProgress.find_elements_by_tag_name("circle")
-        # time = float(circle[1].get_attribute('stroke-dasharray').split(" ")[0])
-        log("Check water process ")
+                #log("More Friends")
+                time.sleep(5)
 
-        waterCanvas = driver.find_element_by_id("waterCanvas")
-        driver.execute_script("arguments[0].click();", waterCanvas)
-        #ActionChains(driver).double_click(waterCanvas).perform()
-        sleep(5)
-    except:
-        log("FULL")
-        driver.close()
-        return 0
 
 
     log("Finished")
-    #pg_src = driver.page_source
-    #print(pg_src)
-    # print(pg_src)
     driver.close()
-    time = 1
-    return time
+    t = 1
+    return t
 
 
 file = open(File_Acc, 'r')
@@ -120,9 +117,15 @@ all_line_txt = file.readlines()
 acc = all_line_txt[0].strip('\n')
 passwd = all_line_txt[1].strip('\n')
 while True:
-    time = do_work(acc, passwd)
+    ## Get Current Time
+    start = time.time()
+    z = do_work(acc, passwd)
+    end = time.time()
+    dur = end - start
+    print("Spent  " + str(dur))
     #print(time)
-    sleep(2 * 60 * 60 + 60)
+    if dur < 2 * 60 * 60 + 60:
+        time.sleep(2 * 60 * 60 + 60 - dur)
 
 
 
